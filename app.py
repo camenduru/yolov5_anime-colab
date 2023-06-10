aa47213aa9e173611a684e38430a98f314ebd181
hysts <hysts@users.noreply.huggingface.co>
2023-06-10 09:22:50 +0000

    Update

--- app.py
+++ app.py
@@ -21,13 +21,9 @@ from models.yolo import Model
 from utils.datasets import letterbox
 from utils.general import non_max_suppression, scale_coords
 
-TITLE = 'zymk9/yolov5_anime'
-DESCRIPTION = 'This is an unofficial demo for https://github.com/zymk9/yolov5_anime.'
+DESCRIPTION = '# [zymk9/yolov5_anime](https://github.com/zymk9/yolov5_anime)'
 
-HF_TOKEN = os.getenv('HF_TOKEN')
-MODEL_REPO = 'hysts/yolov5_anime'
-MODEL_FILENAME = 'yolov5x_anime.pth'
-CONFIG_FILENAME = 'yolov5x.yaml'
+MODEL_REPO = 'public-data/yolov5_anime'
 
 
 def load_sample_image_paths() -> list[pathlib.Path]:
@@ -36,8 +32,7 @@ def load_sample_image_paths() -> list[pathlib.Path]:
         dataset_repo = 'hysts/sample-images-TADNE'
         path = huggingface_hub.hf_hub_download(dataset_repo,
                                                'images.tar.gz',
-                                               repo_type='dataset',
-                                               use_auth_token=HF_TOKEN)
+                                               repo_type='dataset')
         with tarfile.open(path) as f:
             f.extractall()
     return sorted(image_dir.glob('*'))
@@ -46,11 +41,8 @@ def load_sample_image_paths() -> list[pathlib.Path]:
 def load_model(device: torch.device) -> torch.nn.Module:
     torch.set_grad_enabled(False)
     model_path = huggingface_hub.hf_hub_download(MODEL_REPO,
-                                                 MODEL_FILENAME,
-                                                 use_auth_token=HF_TOKEN)
-    config_path = huggingface_hub.hf_hub_download(MODEL_REPO,
-                                                  CONFIG_FILENAME,
-                                                  use_auth_token=HF_TOKEN)
+                                                 'yolov5x_anime.pth')
+    config_path = huggingface_hub.hf_hub_download(MODEL_REPO, 'yolov5x.yaml')
     state_dict = torch.load(model_path)
     model = Model(cfg=config_path)
     model.load_state_dict(state_dict)
@@ -98,25 +90,32 @@ examples = [[path.as_posix(), 0.4, 0.5] for path in image_paths]
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = load_model(device)
-func = functools.partial(predict, device=device, model=model)
+fn = functools.partial(predict, device=device, model=model)
 
-gr.Interface(
-    fn=func,
-    inputs=[
-        gr.Image(label='Input', type='pil'),
-        gr.Slider(label='Score Threshold',
-                  minimum=0,
-                  maximum=1,
-                  step=0.05,
-                  value=0.4),
-        gr.Slider(label='IoU Threshold',
-                  minimum=0,
-                  maximum=1,
-                  step=0.05,
-                  value=0.5),
-    ],
-    outputs=gr.Image(label='Output'),
-    examples=examples,
-    title=TITLE,
-    description=DESCRIPTION,
-).queue().launch(show_api=False)
+with gr.Blocks(css='style.css') as demo:
+    gr.Markdown(DESCRIPTION)
+    with gr.Row():
+        with gr.Column():
+            image = gr.Image(label='Input', type='pil')
+            score_threshold = gr.Slider(label='Score Threshold',
+                                        minimum=0,
+                                        maximum=1,
+                                        step=0.05,
+                                        value=0.4)
+            iou_threshold = gr.Slider(label='IoU Threshold',
+                                      minimum=0,
+                                      maximum=1,
+                                      step=0.05,
+                                      value=0.5)
+            run_button = gr.Button('Run')
+        with gr.Column():
+            result = gr.Image(label='Output')
+
+    inputs = [image, score_threshold, iou_threshold]
+    gr.Examples(examples=examples,
+                inputs=inputs,
+                outputs=result,
+                fn=fn,
+                cache_examples=os.getenv('CACHE_EXAMPLES') == '1')
+    run_button.click(fn=fn, inputs=inputs, outputs=result, api_name='predict')
+demo.queue(max_size=15).launch()
